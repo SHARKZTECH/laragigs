0bab23acfeb66f2722d4c54c5dfed68adf4e3fd1
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-09-28 15:20:54 +0300

    scopefilter by tags

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
-use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
     public function index()  {
         return view('listings.index',[
-            'listings'=>Listing::all()
+            'listings'=>Listing::latest()->filter(request(["tag"]))->get()
         ]);
         
     } 
