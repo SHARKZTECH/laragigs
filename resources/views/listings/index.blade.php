71e44d1fdeef2cda81e86b5bd8f9a560b333539d
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-09-28 14:25:48 +0300

    change layout to comp

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,5 +1,4 @@
-@extends('layout')
-@section('content')
+<x-layout>
     @include('partials._hero')
     @include('partials._search')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
@@ -7,4 +6,4 @@
             <x-listing-card :listing="$listing"></x-listing-card>
         @endforeach
     </div>
-@endsection
+</x-layout>
